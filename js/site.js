ba70dc6a213e1c780b518d15bc4dc5e8bc810bfc
dracher <dracher@gmail.com>
2019-06-27 14:35:22 +0800

    first commit

---
+++ js/site.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".sidenav").sidenav()
+    $(".parallax").parallax()
+    $("select").formSelect()
+    $(".modal").modal()
+    $('.collapsible').collapsible()
+})
